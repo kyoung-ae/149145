9238b6f0152a4c496859b48756fb0c42ac69938d
kyoung-ae <63986079+kyoung-ae@users.noreply.github.com>
2020-05-30 01:16:51 +0900

    Update DB.h

--- DB.h
+++ DB.h
@@ -1,16 +1,17 @@
 #ifndef DB_H
 #define DB_H
 
+#pragma foreign_keys = 1 // 참조키 활성화
+
 // #define len 크기 지정은 실제크기+1 로 지정
-#define SQLlen 512 // input_sql
+#define SQLlen 800 // 입력 길이 제한이 가장 긴 PWDlen(514) 를 포함하는 admin 테이블을 기준으로 설정함
 #define ACCESSlen 2 // int형의 access값을 문자로 받기 위한 변수 크기 -> 크기를 1로 했더니 id[0]을 null로 덮어쓰는 참사 발생 ㅠㅠ
-// access 변수를 int 형에서 2020년 5월 19일 20시경, 문자열로 변경하기로 팀에서 결정함.
 #define WLlen 31
 #define IDlen 10
 #define DATElen 21
 #define PKlen 271
 #define MAClen 18
-#define PWDlen 514 
+#define PWDlen 514
 #define NAMElen 31
 #define EMAILlen 51
 #define PHONElen 21
@@ -56,11 +57,8 @@ struct InsInfo {
     char email[EMAILlen];
     char phone[PHONElen];
     char date[DATElen];
-    char access[ACCESSlen];
-};
-
-enum Account {
-    Robot, CPS, Root
+    char access[ACCESSlen]; // N, E, C, R만 입력 가능함
+    // N은 아무 권한 없음(권한 부여 전의 default), E는 EV3 Robot, C는 CPS, R은 Root 를 나타냄
 };
 
 #endif
