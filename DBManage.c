145afef62cb0f93e2f80a5c9aa06708b0fd72d90
kyoung-ae <63986079+kyoung-ae@users.noreply.github.com>
2020-06-01 11:42:48 +0900

    Update DBManage.c

--- DBManage.c
+++ DBManage.c
@@ -1,29 +1,19 @@
-#define _CTR_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "sqlite3.h"
-#include <string.h>
-#include "BaseDefine.h"
 #include "DB.h"
-#include "DBPrintModule.h"
-#include <ctype.h>
 
-#pragma foreign_keys = 1 // 참조키 활성화
-
-int inAD_INFO(); // case 16 ok
-int inMAC(); // case 17 // 통신과 연결해야해서 작성 못함
-int inWL(); // case 18 ok
-int upAD_INFO(); // case 26 ok
-int upMAC(); // case 27 // 통신과 연결해야해서 작성 못함
-int search_id(); // case 36 // id를 기준으로 검색
-int search_wl(); // case 37 // whitelist를 기준으로 검색
-int delAD_INFO(); // case 46
-int delMAC(); // case 47 // 통신과 연결해야해서 작성 못함
-int delWL(); // case 48
-int searchId(); // case 56 : id 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수
-int searchPwd(char id[]); // case 57 : pwd 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수
-int search_ip(); // case 58 : id와 pwd 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수
+int inAD_INFO(); // DBInsertModule.c : case 16 ok
+int inMAC(); // DBInsertModule.c : case 17 // 통신과 연결해야해서 작성 못함
+int inWL(); // DBInsertModule.c : case 18 ok
+int upAD_INFO(); // DBUpdateModule.c : case 26 ok
+int upMAC(); // DBUpdateModule.c : case 27 // 통신과 연결해야해서 작성 못함
+int search_INFO(); // DBSearchModule.c : case 36 // 관리자 정보를 기준으로 검색
+int searchMAC(); // DBSearchModule.c : case 37 // 공개키와 맥을 기준으로 검색
+int searchWL(); // DBSearchModule.c : case 38 // 화이트리스트를 기준으로 검색
+int delAD_INFO(); // DBDeleteModule.c : case 46 // 아직 작성 못함
+int delMAC(); //  DBDeleteModule.c : case 47 // 통신과 연결해야해서 작성 못함
+int delWL(); //  DBDeleteModule.c : case 48  // 아직 작성 못함
+int searchId(); // DBHintIdPwd.c : case 56 : id 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수 // 아직 작성 못함
+int searchPwd(); // DBHintIdPwd.c : case 57 : pwd 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수 // 아직 작성 못함
+int search_ip(); // DBHintIdPwd.c : case 58 : id와 pwd 분실 시 지원 기능을 위한 관리자 정보를 호출하는 함수 // 작성 못함
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) { // callback
     int i;
