d053dfb7068eecbc70db26377512ff08c9421541
kyoung-ae <63986079+kyoung-ae@users.noreply.github.com>
2020-06-01 11:45:45 +0900

    Update DBPrintModule.h

--- DBPrintModule.h
+++ DBPrintModule.h
@@ -1,9 +1,9 @@
 #ifndef DBPrintModule_H
 #define DBPrintModule_H
 
-#include "DB.h"
-
 void printMenu();
+void print_search_i();
+void print_search_w();
 void printWhiteListResult(struct WhiteListTable pr_wt);
 void printAdminResult(struct InsInfo pr_i);
 
