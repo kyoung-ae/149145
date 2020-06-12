006f9dcd18619f47fc42d0aa9b9f140762b91529
kyoung-ae <63986079+kyoung-ae@users.noreply.github.com>
2020-06-12 10:31:59 +0900

    Delete DBUpdateModule.h

--- DBUpdateModule.h
+++
@@ -1,7 +0,0 @@
-#ifndef DBUpdateModule_H
-#define DBUpdateModule_H
-
-static int callback(void *NotUsed, int argc, char **argv, char **azColName); // callback
-int upAD_INFO(); // case 26 ok
-
-#endif
