ea666771cbebd46cb0fb6281fa86b7c96dea179f
kyoung-ae <63986079+kyoung-ae@users.noreply.github.com>
2020-06-12 10:29:13 +0900

    Delete DBInsertModule.h

--- DBInsertModule.h
+++
@@ -1,8 +0,0 @@
-#ifndef DBManage_H
-#define DBManage_H
-
-static int callback(void *NotUsed, int argc, char **argv, char **azColName); // callback
-int inAD_INFO(); // case 16
-int inWL(); // case 17
-
-#endif
